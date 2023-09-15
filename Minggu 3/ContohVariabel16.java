c11ba179e6ec914f80fca6e96a26070e020687eb
Snapperr <mikaila082@gmail.com>
2023-09-15 14:11:40 +0700

    tugas

---
+++ Minggu 3/ContohVariabel16.java
@@ -0,0 +1,15 @@
+public class ContohVariabel16 {
+    public static void main(String[] args) {
+        String salahsatuhobysayaadalah = "Bermain petak umpet";
+        boolean ispandai = true;
+        char jeniskelamin = 'L';
+        byte umursekarang = 19;
+        double $ipk = 3.24, tinggi = 1.78;
+
+        System.out.println(salahsatuhobysayaadalah);
+        System.out.println("Apakah pandai?"+ispandai);
+        System.out.println("Jenis Kelamin"+jeniskelamin);
+        System.out.println("Umurku saat ini:"+umursekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s",$ipk, tinggi));
+    }
+}
